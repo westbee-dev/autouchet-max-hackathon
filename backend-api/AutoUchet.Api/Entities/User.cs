168010c33cca7dca52abd1779fe5e5cbe1b7f789
sqv1zyy <bulatov.ilya2018@yandex.ru>
2026-09-18 23:31:11 +0500

    feat: Инициализация базовой инфраструктуры API, БД и Swagger
    - Добавлены доменные сущности (Users, Receipts)
    - Реализован класс AppDbContext для подключения к PostgreSQL
    - Созданы и применены начальные миграции базы данных
    - Настроен и подключен Swagger UI

---
+++ backend-api/AutoUchet.Api/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AutoUchet.Api.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public long MaxUserId { get; set; }
+        public decimal TaxRate { get; set; } = 0.04m;
+        public string ActivityType { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public ICollection<Receipt> Receipts { get; set; } = new List<Receipt>();
+    }
+}
