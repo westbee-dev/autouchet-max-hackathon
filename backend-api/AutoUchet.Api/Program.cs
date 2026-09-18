625d7828bd4b11aa6cc9b5bec3b7dccc995bd67a
westbee-dev <stud0000306513@study.utmn.ru>
2026-09-18 23:31:11 +0500

    feat: Инициализация базовой инфраструктуры API, БД и Swagger
    - Добавлены доменные сущности (Users, Receipts)
    - Реализован класс AppDbContext для подключения к PostgreSQL
    - Созданы и применены начальные миграции базы данных
    - Настроен и подключен Swagger UI

---
+++ backend-api/AutoUchet.Api/Program.cs
@@ -0,0 +1,26 @@
+using AutoUchet.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
