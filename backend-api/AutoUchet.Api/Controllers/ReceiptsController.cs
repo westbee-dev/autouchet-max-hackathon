6b077e854bc4a5db98f7da24893238734b91de63
westbee-dev <stud0000306513@study.utmn.ru>
2026-09-21 00:37:00 +0500

    feat: Создание DTO и GET эндпоинтов для главной страницы.
    - Создание ReceiptResponseDto.
    - Создание DashboardResponseDto.
    - Создание контроллера ReceiptsController, а также GET эндпоинта GetReceipts.
    - Создание контроллера DashboardController, а также GET эндпоинта GetDashboard.

---
+++ backend-api/AutoUchet.Api/Controllers/ReceiptsController.cs
@@ -0,0 +1,39 @@
+﻿using AutoUchet.Api.Data;
+using AutoUchet.Api.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoUchet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiptsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ReceiptsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<ReceiptResponseDto>> GetReceipts()
+        {
+            var mappedReceipts = await _context.Receipts
+                .OrderByDescending(r => r.CreatedAt)
+                .Select(
+                r => new ReceiptResponseDto
+                {
+                    Id = r.Id,
+                    Amount = r.Amount,
+                    BuyerType = r.BuyerType,
+                    PurposeOfPayment = r.PurposeOfPayment,
+                    Status = r.Status,
+                    PaymentType = r.PaymentType,
+                    CreatedAt = r.CreatedAt
+                })
+                .ToListAsync();
+
+            return mappedReceipts;
+        }
+    }
+}
