d8c3c3934a91fc304cfe8bea20df39713f6b6150
sqv1zyy <bulatov.ilya2018@yandex.ru>
2026-09-21 16:30:42 +0500

    Добавил проверку доступности токена, запуск Long Polling и обработку сообщений

--- backend-bot/bot/Program.cs
+++ backend-bot/bot/Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using Autouchet_Bot.Handlers;
+using Autouchet_Bot.Keyboards;
 using DotNetEnv;
 using MAX.Bot;
 using MAX.Bot.Interfaces.Models;
 using MAX.Bot.Interfaces.Models.Request.Message;
-using Autouchet_Bot.Keyboards;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Autouchet_Bot;
 
@@ -18,6 +19,29 @@ class Bot
         string botToken = Environment.GetEnvironmentVariable("API_KEY_MAX");
         string miniAppUrl = Environment.GetEnvironmentVariable("MINI_APP_URL");
         var client = new MaxBotClient("botToken");
-       
+        var messageHandler = new MessageHandler(miniAppUrl);
+        var botInfo = await client.GetMeAsync();
+        Console.WriteLine($"Бот запущен: {botInfo.FirstName} (ID: {botInfo.Id})");
+        using var cts = new CancellationTokenSource();
+
+        var _ = client.PollUpdatesWithCallback(
+            async (update, botClient) =>
+            {
+                if (update is MessageCreatedUpdate messageCreated)
+                {
+                    await messageHandler.HandleAsync(messageCreated, client);
+                }
+            },
+            limit: 100,
+            timeout: 90,
+            types: new List<string> { UpdateTypes.MessageCreated },
+            cancellationToken: cts.Token
+        );
+
+        Console.WriteLine("Нажмите Enter для завершения работы бота...");
+        Console.ReadLine();
+        cts.Cancel();
+    }
 }
-}
+
+
