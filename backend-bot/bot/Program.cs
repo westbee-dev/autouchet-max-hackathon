8a63c777f76dc631bbe4783d0442de3f947e0222
sqv1zyy <bulatov.ilya2018@yandex.ru>
2026-09-21 21:40:52 +0500

    Интегрировал ASP.NET Core

--- backend-bot/bot/Program.cs
+++ backend-bot/bot/Program.cs
@@ -2,8 +2,13 @@
 using Autouchet_Bot.Keyboards;
 using DotNetEnv;
 using MAX.Bot;
+using MAX.Bot.Interfaces;
 using MAX.Bot.Interfaces.Models;
 using MAX.Bot.Interfaces.Models.Request.Message;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -18,12 +23,24 @@ class Bot
         Env.Load();
         string botToken = Environment.GetEnvironmentVariable("API_KEY_MAX");
         string miniAppUrl = Environment.GetEnvironmentVariable("MINI_APP_URL");
+
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+
         var client = new MaxBotClient(botToken);
         var messageHandler = new MessageHandler(miniAppUrl);
         var callbackHandler = new CallbackHandler(miniAppUrl);
+
+        builder.Services.AddSingleton<IMaxBotClient>(client);
+        builder.Services.AddSingleton(messageHandler);
+        builder.Services.AddSingleton(callbackHandler);
+
+        var app = builder.Build();
+
         var botInfo = await client.GetMeAsync();
         Console.WriteLine($"Бот запущен: {botInfo.FirstName} (ID: {botInfo.Id})");
         using var cts = new CancellationTokenSource();
+
 
         var _ = client.PollUpdatesWithCallback(
             async (update, botClient) =>
@@ -46,7 +63,7 @@ class Bot
             },
             cancellationToken: cts.Token
         );
-
+        await app.RunAsync();
         Console.WriteLine("Нажмите Enter для завершения работы бота...");
         Console.ReadLine();
         cts.Cancel();
