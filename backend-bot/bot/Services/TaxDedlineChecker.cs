ac5e43735a3e4e9a61f9b7ca2e6b09aae4dc7133
sqv1zyy <bulatov.ilya2018@yandex.ru>
2026-09-23 21:29:42 +0500

    Реализовал класс, в котором расчитывается крайний срок оплаты налогов

---
+++ backend-bot/bot/Services/TaxDedlineChecker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Autouchet_Bot.Services
+{
+    public static class TaxDeadlineChecker
+    {
+        public static DateTime GetTaxDeadline(DateTime date)
+        {
+            DateTime deadline = new DateTime(date.Year, date.Month, 28);
+
+            if (deadline.DayOfWeek == DayOfWeek.Saturday)
+            {
+                deadline = deadline.AddDays(2);
+            }
+            else if (deadline.DayOfWeek == DayOfWeek.Sunday)
+            {
+                deadline = deadline.AddDays(1);
+            }
+
+            return deadline;
+        }
+
+        public static bool IsInTaxNotificationPeriod(DateTime date)
+        {
+            DateTime startDate = new DateTime(date.Year, date.Month, 25);
+            DateTime deadlineDate = GetTaxDeadline(date);
+
+            DateTime currentDate = date.Date;
+
+            return currentDate >= startDate && currentDate <= deadlineDate;
+        }
+    }
+}
